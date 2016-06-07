c0b9ec5cc17b1c0e3d44c07e92fea1e61f69bcf6
Thang Le <thangminh.le208@gmail.com>
2016-06-07 16:14:15 -0400

    adding cached costs
    Former-commit-id: f8fae36af564c3bf18c584144c043e81f92a4c9c

--- config1.py
+++ config1.py
@@ -2,14 +2,20 @@ import theano.tensor as T
 import lasagne
 from lasagne.layers import InputLayer, DenseLayer, Conv2DLayer, Pool2DLayer, DropoutLayer 
 
+import matplotlib.pyplot as plt
+
 from train_conv import load_data, Network
 
-train_data, val_data, test_data = load_data('./data/mnist.pkl.gz')
-train_data = (train_data[0].reshape(50000, 1, 28, 28), train_data[1])
-val_data = (val_data[0].reshape(10000, 1, 28, 28), val_data[1])
+EPOCHS = 10
+DIM = 28
+CLASSES = 10
 
-data_size = (None, 1, 28, 28)
-output_size = 10
+train_data, val_data, test_data = load_data('./data/mnist.pkl.gz')
+train_data = (train_data[0].reshape(50000, 1, DIM, DIM), train_data[1])
+val_data = (val_data[0].reshape(10000, 1, DIM, DIM), val_data[1])
+
+data_size = (None, 1, DIM, DIM)
+output_size = CLASSES
 
 input_var = T.tensor4('input') 
 
@@ -21,10 +27,21 @@ pool1 = Pool2DLayer(conv1, 2)
 conv2 = Conv2DLayer(pool1, 40, 5)
 pool2 = Pool2DLayer(conv2, 2)
 
-fc1 = DenseLayer(pool2, 100)
-dropout = DropoutLayer(fc1, p=0.5)
-out = DenseLayer(dropout, output_size, nonlinearity=lasagne.nonlinearities.softmax)
+fc1 = DenseLayer(pool2, 1000)
+dropout1 = DropoutLayer(fc1, p=0.5)
+
+fc2 = DenseLayer(dropout1, 1000)
+dropout2 = DropoutLayer(fc2, p=0.5)
+
+out = DenseLayer(dropout2, output_size, nonlinearity=lasagne.nonlinearities.softmax)
 
 net = Network(input_var, out)
-net.train(train_data, val_data=val_data, lr=0.03, train_batch_size=10, val_batch_size=100, 
-        epochs = 3)
+train_costs, val_costs = net.train(train_data, val_data=val_data, lr=0.03, train_batch_size=10, 
+                                    val_batch_size=10, epochs = EPOCHS, train_cost_cached=True,
+                                    val_cost_cached=True)
+
+it = range(EPOCHS)
+
+plt.plot(it, train_costs, 'r', it, val_costs, 'b')
+
+plt.show()
--- train_conv.py
+++ train_conv.py
@@ -27,16 +27,17 @@ class Network:
         # defining testing function
         target_var = T.ivector()
         test_prediction = lasagne.layers.get_output(self.out_layer, deterministic=True)
-        test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
+        test_cost = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
 
-        test_loss = test_loss.mean()
+        test_cost = test_cost.mean()
         test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var), dtype=theano.config.floatX)
 
-        self.__test_fn = theano.function([self.input_var, target_var], [test_loss, test_acc])
+        self.__test_fn = theano.function([self.input_var, target_var], [test_cost, test_acc])
         self.__get_preds = theano.function([self.input_var], test_prediction)
     
     def train(self, train_data, val_data=None, lr=0.1, lmbda=0.0,
-              train_batch_size=None, val_batch_size=None, epochs=1):
+              train_batch_size=None, val_batch_size=None, epochs=1,
+              train_cost_cached=False, val_cost_cached=False):
         # extract data and labels
         train_x, train_y = train_data
         val_x, val_y = val_data
@@ -54,39 +55,48 @@ class Network:
         # cost function
         target_var = T.ivector('targets')
         prediction = lasagne.layers.get_output(self.out_layer)
-        loss = lasagne.objectives.categorical_crossentropy(prediction, target_var).mean()
+        cost_fn = lasagne.objectives.categorical_crossentropy(prediction, target_var).mean()
         
         # regularization
         penalty = regularize_network_params(self.out_layer, l2)
-        loss += lmbda * penalty
+        cost_fn += lmbda * penalty
 
         # update rule
         params = lasagne.layers.get_all_params(self.out_layer, trainable=True)
-        updates = lasagne.updates.sgd(loss, params, learning_rate=lr)
+        updates = lasagne.updates.sgd(cost_fn, params, learning_rate=lr)
         
         # defining training and testing function
-        train_fn = theano.function([self.input_var, target_var], loss, updates=updates) 
+        train_fn = theano.function([self.input_var, target_var], cost_fn, updates=updates) 
         
         # training
+        train_costs, val_costs = [], []
+
         for epoch in xrange(epochs):
             for batch in xrange(num_train_batches):
                 iteration = num_train_batches * epoch + batch
                 batch_x = train_x[batch*train_batch_size:(batch+1)*train_batch_size]
                 batch_y = train_y[batch*train_batch_size:(batch+1)*train_batch_size]
-                train_fn(batch_x, batch_y)
+                train_cost = train_fn(batch_x, batch_y)
                 if iteration % 1000 == 0:
                     print("Training mini-batch number {0}".format(iteration))
 
                 if (iteration+1) % num_train_batches == 0:
-                    acc = self.accuracy(val_data, mini_batch_size=val_batch_size)
-                    print("Current test accuracy is {0}%".format(acc * 100))
+                    val_cost, val_acc = self.cost_and_accuracy(val_data, mini_batch_size=val_batch_size)
+                    print("Current test accuracy is {0}%".format(val_acc * 100))
+                    
+                    if train_cost_cached: 
+                        train_costs.append(train_cost)
+                    if val_cost_cached:
+                        val_costs.append(val_cost)
+        
+        return train_costs, val_costs
          
-    def accuracy(self, test_data, mini_batch_size=None):
+    def cost_and_accuracy(self, test_data, mini_batch_size=None):
         test_x, test_y = test_data
         if not mini_batch_size:
             mini_batch_size = test_x.shape[0]
         num_test_batches = test_x.shape[0] / mini_batch_size
         
         return np.mean([self.__test_fn(test_x[i*mini_batch_size:(i+1)*mini_batch_size],
-                                       test_y[i*mini_batch_size:(i+1)*mini_batch_size])[1]
-                        for i in xrange(num_test_batches)]) 
+                                       test_y[i*mini_batch_size:(i+1)*mini_batch_size])
+                        for i in xrange(num_test_batches)], axis=0) 
