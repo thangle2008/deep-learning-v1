9e748a92024ec65a2fa3c11855aff19bd676ef62
Thang Le <thangminh.le208@gmail.com>
2016-06-17 12:13:45 -0400

    add command line arguments for image processing
    Former-commit-id: 66194d8c52717cf077055e57e71fd34dc00c3b65
    Former-commit-id: 7bfcc792283a4d36782a5eff2568faecf1a527f0

--- image-processing/run.py
+++ image-processing/run.py
@@ -1,4 +1,24 @@
 from image_processing import load_image, save_image
+import argparse
 
-data = load_image("/Users/tl9ie/research-2016/image-data/wholedataset", dim=140, expand_train=True, mode="RGB")
-save_image(data, "bird_full_expanded_no_cropped_no_empty_140_rgb.pkl.gz")
+DIM = 140
+EXPAND = False
+IMG_DIR = None
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--img_dir', dest='img_dir')
+parser.add_argument('--expand', dest='expand')
+parser.add_argument('--dim', dest='dim', action="store", type=int)
+
+args = parser.parse_args()
+
+if args.img_dir:
+    IMG_DIR = args.img_dir
+if args.expand:
+    EXPAND = True
+if args.dim:
+    DIM = args.dim
+
+data = load_image(IMG_DIR, dim=DIM, expand_train=EXPAND, mode="RGB")
+save_image(data, "bird_full_no_cropped_no_empty_{0}_rgb.pkl.gz".format(DIM))
